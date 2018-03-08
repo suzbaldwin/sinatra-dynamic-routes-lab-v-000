ce44f2f57e9dfa38de7aaa5bb5a0254d8e25c24a
suzy baldwin <gbaldwin24@comcast.net>
2018-03-08 18:08:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -38,10 +38,10 @@ class App < Sinatra::Base
   get "/:operation/:number1/:number2" do
     a = params[:number1].to_i
     b = params[:number2].to_i
-    
+
    sum = a + b || sum = a - b
     "#{sum}"
-  
+
   end
 
 
